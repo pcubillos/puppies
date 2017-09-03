0c3218fceb838d20494ca5df597b28c74e2d2412
pcubillos <patricio.cubillos@oeaw.ac.at>
2017-09-03 22:11:41 +0200

    Implemented imedit.py, tied together with disk.py into image module.

---
+++ puppies/image/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2018 Patricio Cubillos and contributors.
+# puppies is open-source software under the MIT license (see LICENSE).
+
+from .imedit import *
+from .disk   import *
+
+from .imedit import __all__ as iall
+from .disk   import __all__ as dall
+__all__ = iall + dall
+
+
+# Clean up top-level namespace--delete everything that isn't in __all__
+# or is a magic attribute, and that isn't a submodule of this package
+for varname in dir():
+    if not ((varname.startswith('__') and varname.endswith('__')) or
+            varname in __all__ ):
+        del locals()[varname]
+del(varname)
+
