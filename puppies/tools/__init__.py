88243059d334284b5583f342c98ac41e991c66ec
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-02-18 18:36:45 +0100

    Added parse.py file which implements tools.parse_model() to handle the light-cruve modeling configuration files. It also implements tools.parray().

--- puppies/tools/__init__.py
+++ puppies/tools/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2018 Patricio Cubillos and contributors.
 # puppies is open-source software under the MIT license (see LICENSE).
 
-from .tools import *
+from .tools     import *
+from .parse     import *
 
-from .tools import __all__
+__all__ = tools.__all__ + parse.__all__
 
 # Clean up top-level namespace--delete everything that isn't in __all__
 # or is a magic attribute, and that isn't a submodule of this package
