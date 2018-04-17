1f493987756b1e2cb43cb19412bd88195fc74abc
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-04-17 17:05:01 +0200

    Adapted puppies.models' init to new ramps.py file and eclipse module. Removed mandelecl.py file (replaced with eclipse submodule).

--- puppies/models/__init__.py
+++ puppies/models/__init__.py
@@ -1,26 +1,27 @@
 # Copyright (c) 2018 Patricio Cubillos and contributors.
 # puppies is open-source software under the MIT license (see LICENSE).
 
+import os
+import sys
+
 from .mandeltr  import *
-from .mandelecl import *
-from .linramp   import *
-from .quadramp  import *
-from .expramp   import *
+from .ramps     import *
 from .bliss     import *
+sys.path.append(os.path.dirname(os.path.realpath(__file__))
+                + "/../../modules/eclipse/")
+from eclipse import *
 
 from .mandeltr  import __all__ as mtall
-from .mandelecl import __all__ as meall
-from .linramp   import __all__ as lrall
-from .quadramp  import __all__ as qrall
-from .expramp   import __all__ as erall
+from eclipse    import __all__ as eall
+from .ramps     import __all__ as rall
 from .bliss     import __all__ as biall
 
 # Astrophysical:
-__all__ = (meall + mtall
+__all__ = (mtall + eall
 # Ramps:
-           + lrall + qrall + erall
+         + rall
 # Pixel maps:
-           + biall)
+         + biall)
 
 
 # Clean up top-level namespace--delete everything that isn't in __all__
