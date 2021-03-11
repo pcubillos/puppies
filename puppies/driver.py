47a11cf41aa3067ae31af02c7bd5059cec2066ae
pcubillos <patricio.cubillos@oeaw.ac.at>
2021-03-11 15:08:08 +0100

    PEP8ed puppies/driver.py.

--- puppies/driver.py
+++ puppies/driver.py
@@ -1,8 +1,6 @@
-# Copyright (c) 2018 Patricio Cubillos and contributors.
-# puppies is open-source software under the MIT license (see LICENSE).
+# Copyright (c) 2021 Patricio Cubillos
+# puppies is open-source software under the MIT license (see LICENSE)
 
-import sys
-import os
 import configparser
 
 from . import inst
@@ -10,53 +8,53 @@ from . import tools as pt
 
 
 def init(cfile):
-  """
-  Parse variables from a configuration file into a dictionary.
-  """
-  # Parse variables from a configuration file into a dictionary.
-  config = configparser.ConfigParser()
-  config.optionxform=str
-  config.read([cfile])
+    """
+    Parse variables from a configuration file into a dictionary.
+    """
+    # Parse variables from a configuration file into a dictionary.
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read([cfile])
 
-  if "PUPPIES" not in config.sections():
-    pt.error("Invalid configuration file: '{:s}', no [PUPPIES] section.".
-             format(cfile))
-  # Extract inputs:
-  args = dict(config.items("PUPPIES"))
+    if "PUPPIES" not in config.sections():
+        pt.error(
+            f"Invalid configuration file: '{cfile}', no [PUPPIES] section.")
+    # Extract inputs:
+    args = dict(config.items("PUPPIES"))
 
-  # Check args contains "telescope":
-  if "telescope" not in args.keys():
-    pt.error("Invalid configuration file: '{:s}', no 'telescope' parameter.".
-             format(cfile))
+    # Check args contains "telescope":
+    if "telescope" not in args.keys():
+        pt.error(
+            f"Invalid configuration file: '{cfile}', no 'telescope' parameter.")
 
-  if args["telescope"] == "spitzer":
-    pup = inst.Spitzer(args)
-  elif args["telescope"] == "cheops":
-    pup = None
-  elif args["telescope"] == "jwst":
-    pup = None
+    if args["telescope"] == "spitzer":
+        pup = inst.Spitzer(args)
+    elif args["telescope"] == "cheops":
+        pup = None
+    elif args["telescope"] == "jwst":
+        pup = None
 
-  return pup
+    return pup
 
 
 def run(args):
-  runmode = pt.parray(args["runmode"])
-  nsteps = len(runmode)
+    runmode = pt.parray(args["runmode"])
+    nsteps = len(runmode)
 
-  if runmode[0] == "load":
-    pup = s.Pup(args)
-  else:
-    pup = ls.load(args["pickle"]) # Load object
-    update(pup)
+    if runmode[0] == "load":
+        pup = s.Pup(args)
+    else:
+        pup = ls.load(args["pickle"]) # Load object
+        update(pup)
 
-  for i in np.arange(nsteps):
-    if runmode[i] == "badpix":
-      pbp.badpix(pup)
-    elif runmode[i] == "center":
-      pass
-    elif runmode[i] == "phot":
-      pass
-    elif runmode[i] == "model":
-      pass
-    elif runmode[i] == "mcmc":
-      pass
+    for i in np.arange(nsteps):
+        if runmode[i] == "badpix":
+            pbp.badpix(pup)
+        elif runmode[i] == "center":
+            pass
+        elif runmode[i] == "phot":
+            pass
+        elif runmode[i] == "model":
+            pass
+        elif runmode[i] == "mcmc":
+            pass
