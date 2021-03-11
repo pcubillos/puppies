6bfb9dfc9d9065c2d5f37da33db01538be870a03
pcubillos <patricio.cubillos@oeaw.ac.at>
2021-03-11 16:47:26 +0100

    Renamed io/loadsave.py to io/io.py

--- puppies/io/__init__.py
+++ puppies/io/__init__.py
@@ -1,15 +1,15 @@
-# Copyright (c) 2018 Patricio Cubillos and contributors.
-# puppies is open-source software under the MIT license (see LICENSE).
+# Copyright (c) 2021 Patricio Cubillos
+# puppies is open-source software under the MIT license (see LICENSE)
 
-from .loadsave import __all__
+from .io import __all__
 
-from .loadsave import *
+from .io import *
 
 # Clean up top-level namespace--delete everything that isn't in __all__
 # or is a magic attribute, and that isn't a submodule of this package
 for varname in dir():
     if not ((varname.startswith('__') and varname.endswith('__')) or
-            varname in __all__ ):
+            varname in __all__):
         del locals()[varname]
 del(varname)
 
