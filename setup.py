7c57d809a696b7f4e8d5ce087225fa0eca4ab16b
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2021-03-26 10:24:43 +0100

    Updated setup.py, set lon_description text, set name to exo_puppies (since puppies is already taken on PyPI), and updated email.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import re
+from datetime import date
 import setuptools
 from setuptools import setup, Extension
 
@@ -34,12 +35,41 @@ extensions = [
     for cfile in cfiles
     ]
 
+long_description = f"""
+.. image:: https://raw.githubusercontent.com/pcubillos/puppies/aprilis/docs/figures/logo_puppies_texted.png
+   :width: 50%
+
+The Public Photometry Pipeline for Exoplanets
+
+|Build Status|  |docs|  |PyPI|  |conda|  |License|
+
+:copyright: Copyright {date.today().year} Patricio Cubillos
+:license:   puppies is open-source software under the MIT license
+:URL:       https://puppies.readthedocs.io/
+
+.. |Build Status| image:: https://travis-ci.com/pcubillos/puppies.svg?branch=master
+   :target: https://travis-ci.com/pcubillos/puppies
+
+.. |docs| image:: https://readthedocs.org/projects/puppies/badge/?version=latest
+    :target: https://puppies.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. |PyPI| image:: https://img.shields.io/pypi/v/exo_puppies.svg
+    :target: https://pypi.org/project/exo_puppies/
+    :alt: Latest Version
+
+.. |conda| image:: https://img.shields.io/conda/vn/conda-forge/puppies.svg
+    :target: https://anaconda.org/conda-forge/puppies
+
+.. |License| image:: https://img.shields.io/github/license/pcubillos/puppies.svg?color=blue
+    :target: https://pcubillos.github.io/puppies.html
+"""
 
 setup(
-    name = 'puppies',
+    name = 'exo_puppies',
     version = __version__,
     author = 'Patricio Cubillos',
-    author_email = 'patricio.cubillos@oeaw.ac.at',
+    author_email = 'pcubillos@fulbrightmail.org',
     url = 'https://github.com/pcubillos/puppies',
     packages = setuptools.find_packages(),
     install_requires = [
@@ -53,6 +83,8 @@ setup(
     license = 'MIT',
     description = 'The Public Photometry Pipeline for Exoplanets',
     include_dirs = inc,
+    long_description = long_description,
+    long_description_content_type="text/x-rst",
     entry_points={'console_scripts': ['pup = puppies.__main__:main']},
     ext_modules = extensions,
     )
